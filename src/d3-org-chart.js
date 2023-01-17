d01832c62ae603a572affe2b6f11e11ef260594f
Mohammad Sadegh Hajmohammadi <hajmohammadi.sadegh@gmail.com>
2023-01-17 22:12:37 +0330

    highlite firs div

--- src/d3-org-chart.js
+++ src/d3-org-chart.js
@@ -80,7 +80,7 @@ export class OrgChart {
             },
             nodeUpdate: function (d, i, arr) {
                 d3.select(this)
-                    .select('.node-view')
+                    .select('div')
                     // .select('.node-rect')
                     .attr("stroke", d => d.data._highlighted || d.data._upToTheRootHighlighted ? '#8b95c9' : 'none')
                     .attr("stroke-width", d.data._highlighted || d.data._upToTheRootHighlighted ? 10 : 1)
