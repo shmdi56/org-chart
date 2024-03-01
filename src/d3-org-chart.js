7cf01be0c429f1ff5c0e5b0791a02dbd168b65e7
Sadegh <shmdi56@gmail.com>
2024-03-01 22:18:41 +0330

    isabling zoom

--- src/d3-org-chart.js
+++ src/d3-org-chart.js
@@ -1346,15 +1346,15 @@ export class OrgChart {
     }
 
     zoomTreeBounds({ x0, x1, y0, y1, params = { animate: true, scale: true } }) {
-        const { centerG, svgWidth: w, svgHeight: h, svg, zoomBehavior, duration, lastTransform } = this.getChartState()
-        let scaleVal = Math.min(8, 0.9 / Math.max((x1 - x0) / w, (y1 - y0) / h));
-        let identity = d3.zoomIdentity.translate(w / 2, h / 2)
-        identity = identity.scale(params.scale ? scaleVal : lastTransform.k)
+        // const { centerG, svgWidth: w, svgHeight: h, svg, zoomBehavior, duration, lastTransform } = this.getChartState()
+        // let scaleVal = Math.min(8, 0.9 / Math.max((x1 - x0) / w, (y1 - y0) / h));
+        // let identity = d3.zoomIdentity.translate(w / 2, h / 2)
+        // identity = identity.scale(params.scale ? scaleVal : lastTransform.k)
 
-        identity = identity.translate(-(x0 + x1) / 2, -(y0 + y1) / 2);
-        // Transition zoom wrapper component into specified bounds
-        svg.transition().duration(params.animate ? duration : 0).call(zoomBehavior.transform, identity);
-        centerG.transition().duration(params.animate ? duration : 0).attr('transform', 'translate(0,0)')
+        // identity = identity.translate(-(x0 + x1) / 2, -(y0 + y1) / 2);
+        // // Transition zoom wrapper component into specified bounds
+        // svg.transition().duration(params.animate ? duration : 0).call(zoomBehavior.transform, identity);
+        // centerG.transition().duration(params.animate ? duration : 0).attr('transform', 'translate(0,0)')
     }
 
     fit({ animate = true, nodes, scale = true } = {}) {
